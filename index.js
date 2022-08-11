f13ae78f6152821823d7568bd3e7f2aef45c7d73
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-08-12 00:36:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Creating an array with each object as a flashcard
 let myflashcards = [
     {
         question: "What color is the sky?",
@@ -12,25 +13,34 @@ let myflashcards = [
         answer: "Black and White "
     }]
 
+// Initialises variable i with value 0
 let i = 0;
+
+// Gets the para tag with id q (question) and stores it in a variable 
 let ques = document.getElementById('q');
+// Gets the para tag with id a (answer) and stores it in a variable 
 let ans = document.getElementById('a');
+// Gets the para tag with id Q and stores it in a variable 
 let display = document.getElementById('Q');
+// Gets the para tag with id N and stores it in a variable 
 let next = document.getElementById('N');
 
-display.addEventListener("click",function(){
+//Adds event listener to click and display question 
+display.addEventListener("mouseover",function(){
     // display.textContent="changed";
     ques.innerHTML = a;
   })
-
+// Initializing values of a and b to update them 
 let a='';
 let b='';
 
+//Function that updates the values based on the incremented i value
 function update(){
      a=myflashcards[i].question;
      b=myflashcards[i].answer;
 }
 
+//Iterates over the valies of i and adds event listeners to display question and answers when clicked. 
 while (i < myflashcards.length){
      a=myflashcards[i].question;
      b=myflashcards[i].answer;
